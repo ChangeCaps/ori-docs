3aec06a8f100e9ac3bc32ab9a6c113384d11cf9f
ChangeCaps <42285338+ChangeCaps@users.noreply.github.com>
2024-04-07 19:05:14 +0000

    Deploying to gh-pages from @ ChangeCaps/ori@6cc51a2ea1f45f39d5e48d9dba9569cfba734c84 🚀

--- trait.impl/ori_core/shell/trait.Shell.js
+++ trait.impl/ori_core/shell/trait.Shell.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "ori":[],
-"ori_winit":[["impl Shell for <a class=\"struct\" href=\"ori_winit/struct.WinitShell.html\" title=\"struct ori_winit::WinitShell\">WinitShell</a>"]]
+"ori_winit":[["impl <a class=\"trait\" href=\"ori_core/shell/trait.Shell.html\" title=\"trait ori_core::shell::Shell\">Shell</a> for <a class=\"struct\" href=\"ori_winit/struct.WinitShell.html\" title=\"struct ori_winit::WinitShell\">WinitShell</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
