b23faaa488a8702250b7475ef7ce112847f2fa0e
ChangeCaps <42285338+ChangeCaps@users.noreply.github.com>
2024-04-09 21:06:41 +0000

    Deploying to gh-pages from @ ChangeCaps/ori@d2f9041f0d7add44208194e03e33a00c81467297 🚀

--- ori/prelude/text/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"enum":["FontFamily","FontSource","FontStretch","FontStyle","TextAlign","TextWrap"],"macro":["include_font"],"struct":["FontAtlas","FontWeight","Fonts","RasterizedGlyph","TextAttributes","TextBuffer"]};
