12d236a14a9ed72a0b86dc427f6903b9628bd0a6
ChangeCaps <42285338+ChangeCaps@users.noreply.github.com>
2024-04-20 21:33:32 +0000

    Deploying to gh-pages from @ ChangeCaps/ori@58209d59e815cce544bb0cbba7e96b69123062af 🚀

--- trait.impl/ori_core/window/raw/trait.RawWindow.js
+++ trait.impl/ori_core/window/raw/trait.RawWindow.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "ori":[],
-"ori_winit":[["impl <a class=\"trait\" href=\"ori_core/window/raw/trait.RawWindow.html\" title=\"trait ori_core::window::raw::RawWindow\">RawWindow</a> for <a class=\"struct\" href=\"ori_winit/struct.WinitWindow.html\" title=\"struct ori_winit::WinitWindow\">WinitWindow</a>"]]
+"ori_winit":[["impl RawWindow for <a class=\"struct\" href=\"ori_winit/struct.WinitWindow.html\" title=\"struct ori_winit::WinitWindow\">WinitWindow</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
