30c79a43d2a9273aa2154b945d83a13b4da87f57
ChangeCaps <42285338+ChangeCaps@users.noreply.github.com>
2024-08-12 12:47:00 +0000

    Deploying to gh-pages from @ ChangeCaps/ori@0ae2d2ceb546b479ede264ba1a0d9fb497b67ea9 🚀

---
+++ search.desc/ori_macro/ori_macro-desc-0-.js
@@ -0,0 +1,1 @@
+searchState.loadedDescShard("ori_macro", 0, "Macros for the Ori crate.\nDerive the builder pattern.\nDerive the <code>Rebuild</code> trait.\nEmbed an example in the documentation.\nLoad a font from a file or directory.\nA macro to generate boilerplate for the <code>main</code> function.")
