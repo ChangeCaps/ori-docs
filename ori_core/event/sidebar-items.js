4e53c506755420b20f0371a5736b92be837b4c1d
ChangeCaps <42285338+ChangeCaps@users.noreply.github.com>
2024-08-24 13:18:34 +0000

    Deploying to gh-pages from @ ChangeCaps/ori@3ce0c1fccb1e3f5c0be0aaa2893ddbcc405b70ab 🚀

--- ori_core/event/sidebar-items.js
+++ ori_core/event/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Code","Event","PointerButton"],"struct":["CloseRequested","KeyPressed","KeyReleased","Modifiers","PointerId","PointerLeft","PointerMoved","PointerPressed","PointerReleased","PointerScrolled","WindowResized","WindowScaled"]};
+window.SIDEBAR_ITEMS = {"enum":["Code","Event","Key","PointerButton"],"struct":["CloseRequested","KeyPressed","KeyReleased","Modifiers","PointerId","PointerLeft","PointerMoved","PointerPressed","PointerReleased","PointerScrolled","WindowResized","WindowScaled"],"trait":["IsKey"]};
