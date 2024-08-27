50bd0cd35fb9a917fd2169c0551ff2e6fdc9f73c
ChangeCaps <42285338+ChangeCaps@users.noreply.github.com>
2024-08-27 21:40:29 +0000

    Deploying to gh-pages from @ ChangeCaps/ori@32e38de6a4d824a3ef025eeae947e4402b2e95fd 🚀

--- ori_core/style/sidebar-items.js
+++ ori_core/style/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["palette","style","style_or","styled","try_style"],"struct":["Palette","Styles"],"trait":["Style"]};
+window.SIDEBAR_ITEMS = {"fn":["palette","style","style_or","styled","try_style"],"struct":["Palette","Styles","Theme"],"trait":["Style"]};
