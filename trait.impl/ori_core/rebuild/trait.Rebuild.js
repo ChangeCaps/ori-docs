89c4fa592f3965acde21b8ff86a64d628aabb52a
ChangeCaps <42285338+ChangeCaps@users.noreply.github.com>
2024-08-28 14:17:08 +0000

    Deploying to gh-pages from @ ChangeCaps/ori@77d570339d037b17ac56282bf478b81166ca2fd5 🚀

--- trait.impl/ori_core/rebuild/trait.Rebuild.js
+++ trait.impl/ori_core/rebuild/trait.Rebuild.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
 "ori":[],
 "ori_core":[],
-"ori_font_awesome":[["impl <a class=\"trait\" href=\"ori_core/rebuild/trait.Rebuild.html\" title=\"trait ori_core::rebuild::Rebuild\">Rebuild</a> for <a class=\"struct\" href=\"ori_font_awesome/struct.Icon.html\" title=\"struct ori_font_awesome::Icon\">Icon</a>"]]
+"ori_font_awesome":[["impl Rebuild for <a class=\"struct\" href=\"ori_font_awesome/struct.Icon.html\" title=\"struct ori_font_awesome::Icon\">Icon</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
