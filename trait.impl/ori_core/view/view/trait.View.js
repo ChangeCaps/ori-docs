99d4265610e10c820811b73c061fb689a255b3b2
ChangeCaps <42285338+ChangeCaps@users.noreply.github.com>
2024-08-28 21:43:19 +0000

    Deploying to gh-pages from @ ChangeCaps/ori@c77487d021d022550518bbd3abdfccde5e6dc31f 🚀

--- trait.impl/ori_core/view/view/trait.View.js
+++ trait.impl/ori_core/view/view/trait.View.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {
 "ori":[],
 "ori_core":[],
-"ori_font_awesome":[["impl&lt;T&gt; View&lt;T&gt; for <a class=\"struct\" href=\"ori_font_awesome/struct.Icon.html\" title=\"struct ori_font_awesome::Icon\">Icon</a>"]],
+"ori_font_awesome":[["impl&lt;T&gt; <a class=\"trait\" href=\"ori_core/view/view/trait.View.html\" title=\"trait ori_core::view::view::View\">View</a>&lt;T&gt; for <a class=\"struct\" href=\"ori_font_awesome/struct.Icon.html\" title=\"struct ori_font_awesome::Icon\">Icon</a>"]],
 "ori_reload":[["impl&lt;T, V: <a class=\"trait\" href=\"ori_core/view/view/trait.View.html\" title=\"trait ori_core::view::view::View\">View</a>&lt;T&gt;&gt; <a class=\"trait\" href=\"ori_core/view/view/trait.View.html\" title=\"trait ori_core::view::view::View\">View</a>&lt;T&gt; for <a class=\"struct\" href=\"ori_reload/struct.Watcher.html\" title=\"struct ori_reload::Watcher\">Watcher</a>&lt;V&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
