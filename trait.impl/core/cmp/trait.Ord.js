ff7ebe9084c1da3c603fc9959c59472ee23e5aa9
ChangeCaps <42285338+ChangeCaps@users.noreply.github.com>
2024-09-05 12:04:31 +0000

    Deploying to gh-pages from @ ChangeCaps/ori@bd74b4f709ceaaa69370159f2ac924bc5a648d78 🚀

--- trait.impl/core/cmp/trait.Ord.js
+++ trait.impl/core/cmp/trait.Ord.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"ori_core":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.80.1/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"ori_core/view/struct.ViewId.html\" title=\"struct ori_core::view::ViewId\">ViewId</a>"]]
+"ori_core":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.80.1/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"ori_core/view/struct.ViewId.html\" title=\"struct ori_core::view::ViewId\">ViewId</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.80.1/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"ori_core/views/struct.Void.html\" title=\"struct ori_core::views::Void\">Void</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
