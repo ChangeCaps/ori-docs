b62f920f693468712409944459143262d271e13c
ChangeCaps <42285338+ChangeCaps@users.noreply.github.com>
2024-09-10 00:33:36 +0000

    Deploying to gh-pages from @ ChangeCaps/ori@9e8fb908d54a57d8f18c5681b498024a4fa55621 🚀

--- ori_macro/sidebar-items.js
+++ ori_macro/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"attr":["desktop","example","main","mobile","reloadable","web"],"derive":["Build","Rebuild","Styled"],"macro":["include_font"]};
+window.SIDEBAR_ITEMS = {"attr":["desktop","example","main","mobile","reloadable","web"],"derive":["Build","Rebuild","Styled"],"macro":["include_font","is_desktop","is_mobile","is_web"]};
