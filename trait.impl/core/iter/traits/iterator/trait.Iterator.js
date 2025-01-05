c44ee58e4e2340642cd8825db239d466584b4cb7
ChangeCaps <42285338+ChangeCaps@users.noreply.github.com>
2025-01-05 15:14:57 +0000

    Deploying to gh-pages from @ ori-ui/ori@ad6c67e67203512274690fadff992580842f8dd6 🚀

--- trait.impl/core/iter/traits/iterator/trait.Iterator.js
+++ trait.impl/core/iter/traits/iterator/trait.Iterator.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["ori_core",[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"ori_core/canvas/struct.CurveIter.html\" title=\"struct ori_core::canvas::CurveIter\">CurveIter</a>&lt;'a&gt;"],["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"ori_core/layout/struct.JustifyIterator.html\" title=\"struct ori_core::layout::JustifyIterator\">JustifyIterator</a>&lt;'a&gt;"]]]]);
+    var implementors = Object.fromEntries([["ori_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"ori_core/canvas/struct.CurveIter.html\" title=\"struct ori_core::canvas::CurveIter\">CurveIter</a>&lt;'_&gt;"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"ori_core/layout/struct.JustifyIterator.html\" title=\"struct ori_core::layout::JustifyIterator\">JustifyIterator</a>&lt;'_&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[700]}
+//{"start":57,"fragment_lengths":[680]}
